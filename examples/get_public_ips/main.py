a1324bf8b50503c29ddb8d106841d53fb69eb806
Jason marter <jason.marter@gmail.com>
2024-11-07 10:02:04 -0500

    add pub ip getter

---
+++ examples/get_public_ips/main.py
@@ -0,0 +1,26 @@
+import json
+import asyncio
+from aws_utils.ip_utils import load_config, async_config_query, search_for_ip
+
+async def main():
+    config = load_config()
+    data = await async_config_query(config.get('AGGREGATOR_NAME'), config.get('CONFIG_QUERIES'))
+
+    if config.get('PRINT'):
+        pretty_print_data = json.dumps(data, indent=4)
+        print(pretty_print_data)
+    if config.get('SAVE_FILE'):
+        file = config.get('SAVE_FILE_NAME')
+        with open(file, 'w', encoding='utf-8') as f: # type: ignore
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    if config.get('SEARCH_IP'):
+        matches = search_for_ip(data, config.get('SEARCH_IP'))
+        if matches:
+            print(f'match found for {config.get('SEARCH_IP')}')
+            for match in matches:
+                print(json.dumps(match, indent=4))
+        else:
+            print(f'no matches found for {config.get('SEARCH_IP')}')
+
+if __name__=="__main__":
+    asyncio.run(main())
